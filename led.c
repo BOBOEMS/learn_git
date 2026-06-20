959cb2a51e6f88f4c2bb139be4d4c9c0b115ba5e
boboems <2854568110@qq.com>
2026-06-20 23:31:23 +0800

    led2

--- led.c
+++ led.c
@@ -1,9 +1,9 @@
 int led_init(void)
 {
-    int ret = 0;
-    for(int i = 0; i < 10; i++)
+    uint8_t i = 0;
+    for(i = 0; i < 10; i++)
     {
-        printf("Hello Word ! %d", i);
+        printf("Hello Word ! %d\n", i);
     }
     return 0;
 }
